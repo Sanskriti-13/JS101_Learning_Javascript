453a4f2934f543033e5ea414e4b543888d4a9044
Sanskriti-13 <sanskritim1311@gmail.com>
2022-10-11 00:21:51 +0530

    Day 2

---
+++ Day 2 assignment/Web-23-Conditional-statement/problem4.js
@@ -0,0 +1,15 @@
+let database_user="sanskritim1311@gmail.com";
+let database_pass="123abc";
+
+let user="sanskritim1311@gmail.com";
+let pass="123abc";
+if(database_user==user){
+  if(database_pass==pass){
+    console.log("Login Successful");
+  }
+  else{
+    console.log("Incorrect Password");
+  }
+}else{
+    console.log("Wrong Credentials");
+  }
